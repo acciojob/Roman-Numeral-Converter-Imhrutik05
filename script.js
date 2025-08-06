b7c1c8fe92ced53402b3ac9c935aeb06aa712ee3
Hrutik Chaudhari <hrutikchaudhari05@gmail.com>
2025-08-06 17:22:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -10,6 +10,18 @@ function convertToRoman(num) {
     };
 
   //your code here
+	let values = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+	let symbols = ['M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I'];
+
+	let ans = "";
+	for (let i = 0; i < values.length; i++){
+
+		while (num >= values[i]){
+			num -= values[i];
+			ans = ans + symbols[i];
+		}
+	}
+	return ans;
 
 }
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
